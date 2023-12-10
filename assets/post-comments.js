716891c48cba1296ab937405d98a932f0648cf7b
Pirijan <hi@pirijan.com>
2023-12-10 14:58:48 -0500

    stub comments form and js

---
+++ assets/post-comments.js
@@ -0,0 +1,5 @@
+const summary = document.querySelector('.post-comments-details summary')
+const comments = document.querySelector('.post-comments-details aside')
+
+console.log('🐸🐸🐸🐸🐸🐸🐸🐸🐸🐸', comments, summary)
+summary.innerText = `6 comments`
