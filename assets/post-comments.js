bbaa14b2108f760cb518c9cc5a15dba5ee0ce7ab
Pirijan <hi@pirijan.com>
2023-12-12 12:23:35 -0500

    can create comments

--- assets/post-comments.js
+++ assets/post-comments.js
@@ -1,14 +1,18 @@
 const countElement = document.querySelector('.post-comments-count .comments-string')
 const commentsElement = document.querySelector('.post-comments-container aside')
 const commentsForm = document.querySelector('.post-comments-container form')
-const commentsLoader = document.querySelector('.post-comments-container form .loader')
+
+const commentsCountLoader = document.querySelector('.post-comments-count .loader')
+const commentsFormLoader = document.querySelector('.post-comments-container form .loader')
 
 let slug = window.location.pathname
 slug = slug.replaceAll('/', '')
 slug = slug.replaceAll('.html', '')
 
-const apiHost = "https://kinopio.local:3000" // dev
-// const apiHost = "http://api.kinopio.club" // prod
+const apiHost = 'https://kinopio.local:3000' // dev
+// const apiHost = 'http://api.kinopio.club' // prod
+
+let isSubmitting
 
 const fetchComments = async () => {
   const url = `${apiHost}/personal-blog/comments/${slug}`
@@ -25,9 +29,20 @@ const fetchComments = async () => {
   }
 }
 
+const updateLoadersIsVisible = (value) => {
+  if (value) {
+    commentsCountLoader.classList.remove('hidden')
+    commentsFormLoader.classList.remove('hidden')
+  } else {
+    commentsCountLoader.classList.add('hidden')
+    commentsFormLoader.classList.add('hidden')
+  }
+}
+
 const updateAll = async () => {
-  // TODO isLoading BOOL toggle hidden class on .loader
+  updateLoadersIsVisible(true)
   const comments = await fetchComments()
+  updateLoadersIsVisible(false)
   console.log('🐸 comments', comments)
   comments.forEach(comment => {
     // name
@@ -69,37 +84,55 @@ updateAll()
 
 commentsForm.onsubmit = async (event) => {
   event.preventDefault()
-  const formData = new FormData(commentsForm)
-
-  for (var pair of formData.entries())
-  {
-   console.log('🌺🌺🌺🌺🌺',pair[0]+ ': '+ pair[1]);
+  if (isSubmitting) { return }
+  commentsFormLoader.classList.remove('hidden')
+  isSubmitting = true
+  let body = {}
+  let formData = new FormData(commentsForm)
+  formData.append('postSlug', slug)
+  for (var pair of formData.entries()) {
+    const key = pair[0]
+    let value = pair[1]
+    if (value === 'on') {
+      value = true
+    }
+    body[key] = value
   }
+
+  console.log('💖',body)
 
   try {
+    const url = `${apiHost}/personal-blog/comment/create`
+    let response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    console.log('🌍🌍🌍',data)
 
+    // clear form
+    // success state (w moderation msg)
   } catch (error) {
-
+    console.error('🚒 commentsForm submit', error)
+    // showError()
   }
-
-  // let response = await fetch('/article/formdata/post/user', {
-  //   method: 'POST',
-  //   body: new FormData(formElem)
-  // });
-
-  // let result = await response.json();
-
-  // console.log('🌍🌍🌍',result.message);
-
+  updateLoadersIsVisible(false)
+  isSubmitting = false
 }
 
-// const create = async (event) => {
-  // event.preventDefault()
-// }
-
-// console.log(commentsForm)
-// commentsForm.addEventListener('click', create)
 
 
+// global cache.js
 // restore commentPreferences from cache (name, email, ..)
 // restore inProgressComment for slug (save on change, clear on submit)
+
+// progressive moderation?
+
+// is awaiting verification??
+// is awaiting moderation??
+
+
+// send emails: new postmark server: blog-comments
