c3f1c1c864b6633fc1a2bc738541153c59885c1f
Pirijan <hi@pirijan.com>
2023-12-19 12:44:37 -0500

    change apihosts to prod

--- assets/post-comments.js
+++ assets/post-comments.js
@@ -18,8 +18,8 @@ let slug = window.location.pathname
 slug = slug.replaceAll('/', '')
 slug = slug.replaceAll('.html', '')
 
-const apiHost = 'https://kinopio.local:3000' // dev
-// const apiHost = 'http://api.kinopio.club' // prod
+// const apiHost = 'https://kinopio.local:3000' // dev
+const apiHost = 'http://api.kinopio.club' // prod
 
 let isSubmitting
 
--- assets/subscribe-by-email.js
+++ assets/subscribe-by-email.js
@@ -1,7 +1,7 @@
 const forms = document.querySelectorAll('.subscribe-by-email-form')
 
-const apiHost = 'https://kinopio.local:3000' // dev
-// const apiHost = 'http://api.kinopio.club' // prod
+// const apiHost = 'https://kinopio.local:3000' // dev
+const apiHost = 'http://api.kinopio.club' // prod
 
 let savingBadge, successBadge, errorBadge, resultDescription
 
