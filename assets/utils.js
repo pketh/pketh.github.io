ee78ad77caffb79bcc7b7a4add4e79a9577d4d55
Pirijan <hi@pirijan.com>
2023-12-19 13:07:08 -0500

    fix apihost url

--- assets/utils.js
+++ assets/utils.js
@@ -1,4 +1,4 @@
 // loads before any other js
 
 // const apiHost = 'https://kinopio.local:3000' // dev
-const apiHost = 'http://api.kinopio.club' // prod
+const apiHost = 'https://api.kinopio.club' // prod
