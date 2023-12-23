5e6c53cc787031c8b383912ed9a871b4dbbb3e10
Pirijan <hi@pirijan.com>
2023-12-23 10:16:23 -0500

    fix mobile comment inputs, disable safari ios autozoom

---
+++ assets/init.js
@@ -0,0 +1,27 @@
+// loads before any other js
+
+// const apiHost = 'https://kinopio.local:3000' // dev
+const apiHost = 'https://api.kinopio.club' // prod
+
+// https://stackoverflow.com/a/57527009
+const disableIosTextFieldZoom = () => {
+  if (!isIOS()) { return }
+  const element = document.querySelector('meta[name=viewport]')
+  if (element !== null) {
+    let content = element.getAttribute('content')
+    let scalePattern = /maximum\-scale=[0-9\.]+/g
+    if (scalePattern.test(content)) {
+      content = content.replace(scalePattern, 'maximum-scale=1.0')
+    } else {
+      content = [content, 'maximum-scale=1.0'].join(', ')
+    }
+    element.setAttribute('content', content)
+  }
+}
+
+// https://stackoverflow.com/questions/9038625/detect-if-device-is-ios/9039885#9039885
+const isIOS = () => {
+  /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
+}
+
+disableIOSTextFieldZoom()
--- assets/utils.js
+++
@@ -1,4 +0,0 @@
-// loads before any other js
-
-// const apiHost = 'https://kinopio.local:3000' // dev
-const apiHost = 'https://api.kinopio.club' // prod
